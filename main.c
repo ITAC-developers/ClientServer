ccd781aca8e22b37748ecaa51781b0e7b4846062
Андрей Спичкин <90407893+SpeechKingDusha@users.noreply.github.com>
2022-04-06 15:29:46 +0300

    it is good

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-#include "fucking.h"
 
 int is_Lacomij_kusochek (int one_part, int second_part);
 
-struct BigKrasavchik_t {
+struct BigKrasavchik_t 
+{
 	int size_left_hand;
 	int size_right_hand;
 };
 int main ()
 {
-unsigned long long Krasavchik;
-struct BigKrasavchik_t bigKrasavchik;
-is_Lacomij_kusochek (135, 115);
+	struct BigKrasavchik_t bigKrasavchik;
+	
+	bigKrasavchik.size_left_hand =135;
+	bigKrasavchik.size_right_hand = 115;
+
+	return is_Lacomij_kusochek (bigKrasavchik.size_left_hand, bigKrasavchik.size_right_hand);
 }
 
 
 int is_Lacomij_kusochek (int one_part, int second_part)
 {
- return one_part+second_part;
+	 return one_part+second_part;
 }
