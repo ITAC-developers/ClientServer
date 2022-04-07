a42c4bc6c30ed74a7a1ec580b67d8276df31da4a
i.moiseev <i.moiseev@iocorp.ru>
2022-04-07 17:20:58 +0300

    Add gtests

---
+++ ITAC/Server/Tests/Server_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include "Server.h"
+
+TEST(Server, Status)
+{
+    using namespace itac::server;
+    Server server(8);
+    ASSERT_EQ(server.GetStatus(), ServerState::CONNECTING);
+}
+
