f22fa93cfdba758f50e830dbfed1b4aa8531f47d
i.moiseev <i.moiseev@iocorp.ru>
2022-04-11 11:01:01 +0300

    Remove gtest config to main CMakeLists

--- ITAC/Server/Tests/Server_test.cpp
+++ ITAC/Server/Tests/Server_test.cpp
@@ -6,6 +6,5 @@ TEST(Server, Status)
     using namespace itac::server;
     Server server(8);
     ASSERT_EQ(server.GetStatus(), ServerState::CONNECTING);
-    ASSERT_EQ(server.GetStatus(), ServerState::ERROR);
 }
 
