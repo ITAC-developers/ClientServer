e3a111d87b80d3f70025ba345d8565d262cab708
i.moiseev <i.moiseev@iocorp.ru>
2022-04-15 15:26:33 +0300

    Add comment to own_literals

--- ITAC/Common/inc/own_literals.h
+++ ITAC/Common/inc/own_literals.h
@@ -2,8 +2,13 @@
 
 #include <filesystem>
 
+/**
+ * @brief String literal _p for create std::filesystem::path object
+ * @param path C-style string that contain path
+ * @param sz   size of path
+ * @return     new std::filesystem::path
+ */
 inline std::filesystem::path operator "" _p(const char *path, std::size_t sz)
 {
     return {path, path + sz};
 }
-
