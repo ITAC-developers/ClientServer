0082bb796c7ed7c9e7b898f6cf64ca5f44385781
i.moiseev <i.moiseev@iocorp.ru>
2022-04-17 21:18:43 +0300

    Add comment

--- ITAC/Common/inc/Logger.h
+++ ITAC/Common/inc/Logger.h
@@ -8,47 +8,107 @@
 #include <iostream>
 #include <mutex>
 #include <string>
+#include <vector>
 
 #include <own_literals.h>
 
 namespace ITAC::common
 {
 
+/**
+ * @brief Callback function that reset output in Logger, if ostream was destroyed
+ * @param ev      Event of stream
+ * @param stream  Object of stream that rise event
+ * @param index   I don't no
+ */
 void ostream_callback(std::ios::event ev, std::ios_base& stream, int index);
 
-//TODO close file in destructor
 //Test:
-//1 - Test GetInstance() - DONE
 //2 - Test SetLvl()
 //3 - Test SetOutput()
-//4 - Test Log() - DONE
 //5 - Add function SetSplitter that split output arguments with given string
 //6 - Test SetSplitter
+//7 - Test PraseLogLine
 
+
+/**
+ * Class loger provide easy way to centralization logging
+ * It's a thread-safe singelton class
+ */
 class Logger {
 public:
+
+    /**
+     * Levels of log enum
+     */
     enum class LVL
     {
-        TRC, //for trace massage, like input or output parameters, counter etc.
-        DBG, //for debug massage, like start/end functions, create/destroy objects etc.
-        INF, //for information massage, like start server, config state etc.
-        WRN, //for warning message, like non-critical errors
-        ERR  //for error message, like error that make it impossible to normally continue app.
+        TRC, /** For trace massage, like input or output parameters, counter etc. */
+        DBG, /** For debug massage, like start/end functions, create/destroy objects etc. */
+        INF, /** For information massage, like start server, config state etc. */
+        WRN, /** for warning message, like non-critical errors */
+        ERR  /** for error message, like error that make it impossible to normally continue app. */
     };
 
+    /** Logger is singelton, copy/move constructor/assigment operator deleted */
     Logger(const Logger &) = delete;
     Logger(Logger &&) = delete;
     Logger &operator=(const Logger &) = delete;
     Logger &operator=(Logger &&) = delete;
 
+    /**
+     * @brief  Get instance of logger. In first time this function call constructor
+     *         and return pointer to created instance of logger
+     *         In next calls it return pointer in exists instance of logger
+     * @return Pointer to the Logger instance
+     */
     static Logger *GetInstance();
 
+    /**
+     * @brief Set logger level. If function Log called with level lower that logger level,
+     *         Log function immediately return without white log
+     * @param lvl Any level from LVL enum that be set
+     */
     void SetLvl(LVL lvl);
+
+    /**
+     * @brief Set output stream. This function register callback for event when
+     *        stream is destroyed
+     * @param out Any opened stream.
+     */
     void SetOutput(std::ostream& out);
+
+    /**
+     * @brief Set output file for log. Create all directories if it's not exists
+     * @param path Path for opened log file.
+     */
     void SetOutput(const std::filesystem::path& path);
+
+    /**
+     * @brief Get string representation of current output
+     * @return std::cout" || "std::stream" || output file name
+     */
     std::string GetOutput();
+
+    /**
+     * @brief Get current output level
+     * @return Current level
+     */
     LVL GetLvl();
 
+    /**
+     * @brief Add log line to current output. Each of log line have next format:
+     *        [time] [lvl] {func: line} message. This function can construct
+     *        message from any count and any type of arguments. But all of args
+     *        must be printable (it must have overloaded of operator<< to ostream)
+     * @tparam Args Variadic template type
+     * @param lvl   Level of message
+     * @param func  Function where this method called (use macro from this file
+     *              for auto filling this value, or use __func__)
+     * @param line  Line where this method called (use macro from this file for
+     *              auto filling this value, ot use __LINE__)
+     * @param args  Any printable args, that will be concat to message
+     */
     template<typename ...Args>
     void Log(LVL lvl, const std::string &func, unsigned line, Args... args)
     {
@@ -69,15 +129,16 @@ private:
 
 private:
     Logger() = default;
+    ~Logger();
 
-    void CheckOutput();
-
+    /** This function needed when template args have only one value */
     template<typename T>
     void InnerLog(T t)
     {
         *m_output << t << std::endl;
     }
 
+    /** Recursive function for any count of args */
     template<typename T, typename ...Args>
     void InnerLog(T t, Args... args)
     {
@@ -89,7 +150,6 @@ private:
 
     void OpenLogFile();
     void CloseLogFile();
-
 };
 
 struct LogLine {
@@ -100,16 +160,18 @@ struct LogLine {
     unsigned line;
 };
 
-LogLine ParseLogLine(const std::string &line);
+/**
+ * @brief Create vector of LogLine structure's - one structure for each line
+ *        Split input line by '\n'
+ * @param line Input line. It can contain many lines separate by '\n'
+ * @return
+ */
+std::vector<LogLine> ParseLogLines(const std::string &lines);
 
-} // namespace ITAC::common
-
-namespace ITAC
-{
 #define TRC(...) common::Logger::GetInstance()->Log(ITAC::common::Logger::LVL::TRC, __func__, __LINE__, __VA_ARGS__)
 #define DBG(...) common::Logger::GetInstance()->Log(ITAC::common::Logger::LVL::DBG, __func__, __LINE__, __VA_ARGS__)
 #define INF(...) common::Logger::GetInstance()->Log(ITAC::common::Logger::LVL::INF, __func__, __LINE__, __VA_ARGS__)
 #define WRN(...) common::Logger::GetInstance()->Log(ITAC::common::Logger::LVL::WRN, __func__, __LINE__, __VA_ARGS__)
 #define ERR(...) common::Logger::GetInstance()->Log(ITAC::common::Logger::LVL::ERR, __func__, __LINE__, __VA_ARGS__)
-}
 
+} // namespace ITAC::common
