b410123f30a56c40380401aab26278f57700812b
Andrew <dusha.fl2016@yandex.ru>
2022-04-25 22:05:38 +0300

    Created viewmodel and model for authorizate

---
+++ clients/ITACmessendgerMVVM(wpf)/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ITACmessendgerMVVM_wpf_.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITACmessendgerMVVM_wpf_.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        authorizateForm authoForm;
+        public authorizateForm AuthoForm { get; set; }
+        public MainWindowViewModel()
+        {
+            AuthoForm = new authorizateForm();
+            AuthoForm.Login = "andreysp";
+            AuthoForm.Password = "Password";
+            AuthoForm.IsAuthirizated = false;
+            AuthoForm.IsRemembered = true;
+            AuthoForm.MsgHint = "Server not founded";
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyData)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyData));
+        }
+    }
+}
