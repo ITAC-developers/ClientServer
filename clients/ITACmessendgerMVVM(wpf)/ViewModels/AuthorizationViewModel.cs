ba5bc804e9f05100a30bb33ac13d7605804b2343
Андрей Спичкин <dusha.fl2016@yandex.ru>
2022-05-03 21:38:50 +0300

    Edited binding mode in loginbox with model on twoWay

--- clients/ITACmessendgerMVVM(wpf)/ViewModels/AuthorizationViewModel.cs
+++ clients/ITACmessendgerMVVM(wpf)/ViewModels/AuthorizationViewModel.cs
@@ -11,33 +11,18 @@ namespace ITACmessendgerMVVM_wpf_.ViewModels
     class AuthorizationViewModel : INotifyPropertyChanged
     {
         authorizateForm authoForm;
-        client clientITAC;
         public authorizateForm AuthoForm
         {
             get { return authoForm; }
             private set { authoForm = value; }
         }
-        public client ClientITAC
-        {
-            get { return clientITAC; }
-            private set { clientITAC = value; }
-        }
         public AuthorizationViewModel()
         {
-            clientITAC = new client();
-            clientITAC.HostName = "127.0.0.1";
-            clientITAC.PortServer = 8005;
-            clientITAC.connectToSetver();
-
             AuthoForm = new authorizateForm();
             authoForm.Login = "Enter username";
             authoForm.Password = "Password";
             authoForm.IsAuthirizated = false;
             authoForm.IsRemembered = false;
-            authoForm.MsgHint = clientITAC.Answer;
-
-
-            clientITAC.sendMsg("login:" + authoForm.Login + ". Password:" + authoForm.Password);
         }
         public event PropertyChangedEventHandler PropertyChanged;
         void OnPropertyChanged(string propertyData)
